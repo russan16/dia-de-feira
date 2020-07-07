c8e67f17511086dfcf7d569f1d061946313ec30c
Italo Russan <italo.russan@csktech.com.br>
2020-07-07 09:57:32 -0300

    add: ajuste do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <section className="container bg-dark pt-2 pb-4">
+            <Header/>
+            <div className="App">
+            </div>
+        </section>
+    );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header className="jumbotron bg-secondary">
+            <h1 className="text-light">Dia de feira</h1>
+        </header>
+    );
+}
